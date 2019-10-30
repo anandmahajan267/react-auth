a40a959841ab694abafce161115a120c814e2d43
Anand Mahajan <anandmahajan267@gmail.com>
2019-10-30 11:11:29 +0530

    Add codeowners

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { Link as RouterLink } from 'react-router-dom';
@@ -29,11 +27,9 @@ export default () => {
         <div className={classes.root}>
             <AppBar position="static">
                 <Toolbar>
-                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                        <MenuIcon />
-                    </IconButton>
+                   
                     <Typography variant="h6" className={classes.title}>
-                        React Auth App
+                    <Link color="inherit" component={LinkComponent} className={classes.link} to='/'> React Auth App</Link>
           </Typography>
 
                     <Link color="inherit" component={LinkComponent} className={classes.link} to='/login'>Login </Link>
